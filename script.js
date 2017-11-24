0edd31812b095d00531a3b471063ed63ba82805e
phillip shields <efe-in-the-mountains@users.noreply.github.com>
2017-11-25 00:06:29 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,165 @@
+var dictionary = [{
+        definition: "n. A statesman who is enamoured of existing evils, as distinguished from the Liberal, who wishes to replace them with others.",
+        word: "Conservative"
+      },
+    {
+        definition: "n. A blackguard whose faulty vision sees things as they are, not as they ought to be. Hence the custom among the Scythians of plucking out a cynic's eyes to improve his vision.",
+        word: "Cynic"
+      },
+    {
+        definition: "n. A person of low taste, more interested in himself than in me.",
+        word: "Egotist"
+      },
+    {
+        definition: "n. Belief without evidence in what is told by one who speaks without knowledge, of things without parallel.",
+        word: "Faith"
+      },
+    {
+        definition: "n. The seat of misgovernment. That which provides the fire, the pot, the dinner, the table and the knife and fork for the anarchist; the part of the repast that himself supplies is the disgrace before meat. Capital Punishment, a penalty regarding the justice and expediency of which many worthy persons—including all the assassins—entertain grave misgivings.",
+        word: "Capital"
+      },
+    {
+        definition: "n. The period of human life intermediate between the idiocy of infancy and the folly of youth—two removes from the sin of manhood and three from the remorse of age.",
+        word: "Childhood"
+      },
+    {
+        definition: "n. A place where horses, ponies and elephants are permitted to see men, women and children acting the fool.",
+        word: "Circus"
+      },
+    {
+        definition: "n. A body of men who meet to repeal laws.",
+        word: "Congress"
+      },
+    {
+        definition: "n. In American politics, a person who having failed to secure an office from the people is given one by the Administration on condition that he leave the country.",
+        word: "Consul"
+      },
+    {
+        definition: "n. A fair for the display of the minor mental commodities, each exhibitor being too intent upon the arrangement of his own wares to observe those of his neighbor.",
+        word: "Conversation"
+      },
+    {
+        definition: "n. A person who boasts himself hard to please because nobody tries to please him.",
+        word: "Critic"
+      },
+    {
+        definition: "n. An ingenious substitute for the chain and whip of the slave-driver.",
+        word: "Debt"
+      },
+    {
+        definition: "adj. Less conspicuously admirable than one's ancestors. The contemporaries of Homer were striking examples of degeneracy; it required ten of them to raise a rock or a riot that one of the heroes of the Trojan war could have raised with ease. Homer never tires of sneering at men who live in these degenerate days, which is perhaps why they suffered him to beg his bread—a marked instance of returning good for evil, by the way, for if they had forbidden him he would certainly have starved.",
+        word: "Degenerate"
+      },
+    {
+        definition: "n. The father of a most respectable family, comprising Enthusiasm, Affection, Self-denial, Faith, Hope, Charity and many other goodly sons and daughters",
+        word: "Delusion"
+      },
+    {
+        definition: "n. A tyrant's authority for crime and fool's excuse for failure.",
+        word: "Destiny"
+      },
+    {
+        definition: "n. A malevolent literary device for cramping the growth of a language and making it hard and inelastic. This dictionary, however, is a most useful work.",
+        word: "Dictionary"
+      },
+    {
+        definition: "n. The silver lining to the cloud of servitude.",
+        word: "Disobedience"
+      },
+    {
+        definition: "n. One skilled in circumvention of the law.",
+        word: "Lawyer"
+      },
+    {
+        definition: "n. A household consisting of a master, a mistress, and two slaves, making in all, two.",
+        word: "Marriage"
+      },
+    {
+        definition: "n. A daughter of Hope and Fear, explaining to Ignorance the nature of the Unknowable.",
+        word: "Religion"
+      },
+    {
+        definition: "n. The Period of Possibility, when Archimedes finds a fulcrum, Cassandra has a following and seven cities compete for the honor of endowing a living Homer. Youth is the true Saturnian Reign, the Golden Age on earth again, when figs are grown on thistles, and pigs betailed with whistles and, wearing silken bristles, live ever in clover, and cows fly over, delivering milk at every door, and Justice is never heard to snore, and every assassin is made a ghost and, howling, is cast into Baltimost! —Polydore Smith",
+        word: "Youth"
+      },
+    {
+        definition: " adj. Not conforming to standard. In matters of thought and conduct, to be independent is to be abnormal, to be abnormal is to be detested. Wherefore the lexicographer adviseth a striving toward the straiter [sic] resemblance of the Average Man than he hath to himself. Whoso attaineth thereto shall have peace, the prospect of death and the hope of Hell.",
+        word: "Abnormal,"
+      },
+    {
+        definition: "n. [from ACADEME] A modern school where football is taught.",
+        word: "Academy"
+      },
+    {
+        definition: "n. An inevitable occurrence due to the action of immutable natural laws.",
+        word: "Accident"
+      },
+    {
+        definition: "n. An instrument in harmony with the sentiments of an assassin.",
+        word: "Accordion"
+      },
+    {
+        definition: "n. The mother of caution.",
+        word: "Accountability"
+      },
+    {
+        definition: "n. A person whom we know well enough to borrow from, but not well enough to lend to. A degree of friendship called slight when its object is poor or obscure, and intimate when he is rich or famous.",
+        word: "Acquaintance"
+      },
+    {
+        definition: "n. In international politics, the union of two thieves who have their hands so deeply inserted in each other's pockets that they cannot separately plunder a third",
+        word: "Alliance"
+      },
+    {
+        definition: "adj. In bad company.",
+        word: "Alone"
+      },
+    {
+        definition: "n. A convenient deity invented by the ancients as an excuse for getting drunk.",
+        word: "Bacchus"
+      },
+    {
+        definition: "n. A preparation that renders the hook more palatable. The best kind is beauty.",
+        word: "Bait"
+      },
+    {
+        definition: "n. A method of untying with the teeth of a political knot that would not yield to the tongue.",
+        word: "Battle"
+      },
+    {
+        definition: "n. The power by which a woman charms a lover and terrifies a husband.",
+        word: "Beauty"
+      },
+    {
+        definition: "n. The first and direst of all disasters. As to the nature of it there appears to be no uniformity. Castor and Pollux were born from the egg. Pallas came out of a skull. Galatea was once a block of stone. Peresilis, who wrote in the tenth century, avers that he grew up out of the ground where a priest had spilled holy water. It is known that Arimaxus was derived from a hole in the earth, made by a stroke of lightning. Leucomedon was the son of a cavern in Mount Aetna, and I have myself seen a man come out of a wine cellar.",
+        word: "Birth"
+      },
+    {
+        definition: "n. An apparatus with which we think what we think. That which distinguishes the man who is content to be something from the man who wishes to do something. A man of great wealth, or one who has been pitchforked into high station, has commonly such a headful of brain that his neighbors cannot keep their hats on. In our civilization, and under our republican form of government, brain is so highly honored that it is rewarded by exemption from the cares of office.",
+        word: "Brain"
+      },
+    {
+        definition: "n. A woman with a fine prospect of happiness behind her.",
+        word: "Bride"
+      },
+    {
+        definition: " n. See HUSBAND",
+        word: "Brute"
+      }
+
+    ];
+
+var defDisplay = document.getElementById("newDef");
+var wordDisplay = document.getElementById("newWord");
+var quoteBtn = document.getElementById("quoteBtn");
+
+
+quoteBtn.addEventListener("click", function(){
+  newQuote();
+})
+
+function newQuote() {
+    var random = Math.floor(Math.random() * dictionary.length);
+    defDisplay.textContent = dictionary[random].definition;
+    wordDisplay.textContent = "~" + dictionary[random].word;
+}
